f72d1167aeec6c73b25a6b76d2ddf12a85c89900
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2026-02-10 12:57:36 -0500

    Add .gitignore, app.py, and requirements.txt files to configure the environment and server app

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+load_dotenv()
+CORS(app)
+
+API_KEY = os.getenv("GEMINI_API_KEY")
+
+if not API_KEY:
+    # Esto es mejor imprimirlo en consola que lanzar raise error para que Render no crashee en el build
+    print("GEMINI_API_KEY environment variable not set")
+
+else:
+    genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel("gemini-2.5-flash",
+                              system_instruction="Eres una inteligencia artificial de entrenamiento para soldados espaciales"
+                              "responde a los soldados de la organización y exígeles lo mejor de ellos mismos.")
+
+@app.route("/askai", methods=["POST"])
+
+def ask_ai():
+    if not API_KEY:
+        return jsonify({"error": "GEMINI_API_KEY environment variable not set"}), 500
+    
+    try:
+        data = request.get_json()
+        prompt = data.get("prompt", "")
+
+        if not prompt:
+            return jsonify({"error": "Prompt is required"}), 400
+        
+        response = model.generate_content(prompt)
+
+        return jsonify({"response": response.text}), 200 #OK
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500 #Internal Server Error
+
+    #prueba local
+    #Invoke-RestMethod -Uri "http://localhost:10000/askai" -Method Post -ContentType "application/json; charset=utf-8" -Body '{"prompt": "Señor, reporte de situación."}'
+    #curl -X POST http://localhost:10000/askai \-H "Content-Type: application/json" \-d '{"prompt": "Señor, solicito instrucciones."}'
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
